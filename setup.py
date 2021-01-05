cdaa358abf4218b070bb1c206ba5e80edb131e2d
vladsaveliev <vladislav.sav@gmail.com>
2021-01-05 14:58:59 +1100

    Drop versionpy, click, requirements. Use bumpversion

--- setup.py
+++ setup.py
@@ -3,30 +3,18 @@ from setuptools import setup
 
 pkg = 'tsvtools'
 
-try:
-    import versionpy
-except ImportError:
-    res = input('Installation requires versionpy. Install it now? [Y/n]')
-    if res.lower().startswith('n'):
-        raise
-    os.system('pip install versionpy')
-    import versionpy
-
-version = versionpy.get_version(pkg)
-
 setup(
     name=pkg,
-    version=version,
-    author='Vlad Saveliev',
+    version='0.1.0',
+    author='Vlad Savelyev',
     author_email='vladislav.sav@gmail.com',
     description='Utilities for operating with tab-separated files: viewing, filtering, reordering',
-    keywords='bioinformatics',
+    keywords=['bioinformatics', 'data science'],
     url='https://github.com/vladsaveliev/' + pkg,
     license='GPLv3',
     packages=[pkg],
     include_package_data=True,
     zip_safe=False,
-    install_requires=['click', 'versionpy'],
     scripts=['scripts/tsvtools', 'scripts/tsv', 'scripts/cols'],
     classifiers=[
         'Environment :: Console',
